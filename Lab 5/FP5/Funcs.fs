c5a76b3d55bee7517cb270d1edb354412e2cac92
Melchalk <melm2004@mail.ru>
2025-02-14 18:43:59 +0300

    fix lab 5

--- Lab 5/FP5/Funcs.fs
+++ Lab 5/FP5/Funcs.fs
@@ -6,7 +6,11 @@ let getDifference (x:int) (y:int) : int = x - y
 
 let getMultiplication (x:int) (y:int) : int = x * y
 
-let getDivision x y = x / y
+let getDivision x y =
+    if y <> 0.0 then x / y
+    else 
+        printfn "Ошибка: деление на ноль"
+        0.0
 
 let rec getFactorial x =
     if x = 0 then 1
